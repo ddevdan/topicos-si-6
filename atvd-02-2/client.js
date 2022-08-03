65711d559e951b483bc1a829cb21dca0691bf62d
= <dnc331@gmail.com>
2022-08-02 23:06:20 -0300

    created question 02-2 server.js and client.js

---
+++ atvd-02-2/client.js
@@ -0,0 +1,16 @@
+const dgram = require('dgram');
+const { Buffer } = require("buffer")
+const { sendUdpMessage } = require("../utils");
+
+const client = dgram.createSocket('udp4');
+
+
+client.on('message', (msg, rinfo) => {
+  console.log(`${msg}`);
+});
+
+
+client.connect(8080, () => {
+  console.log("Digite seu nome!")
+  sendUdpMessage(client)
+})
